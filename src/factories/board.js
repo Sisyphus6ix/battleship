dfadb680d10b85095d89932f7e16abbfbbc151c1
Ryan Silvera <ryan.silvera@icloud.com>
2023-04-19 18:59:04 -0400

    Cleaning up recieveAttack function

--- src/factories/board.js
+++ src/factories/board.js
@@ -10,15 +10,13 @@ export function Board (size) {
     // Empty array for storing spaces on the board
     this.boardSpaces = []
 
-    // Function for placing the coordinates of a ship into my boardSpaces array 
+    // Function for placing the coordinates of a ship and that ship into my boardSpaces array 
     // as well as the ship into my fleet array
     const placeShip = function (theShip) {
         for (let i = 0; i < theShip.coords.length; i++){
             let x = theShip.coords[i].getX()
             let y = theShip.coords[i].getY()
-
-            // console.log({x, y})
-            this.boardSpaces.push({x, y})
+            this.boardSpaces.push({x, y, theShip})
         }
         this.fleet.push(theShip)
     }
@@ -36,20 +34,8 @@ export function Board (size) {
             // console.log(space)
             if (space.x == x && space.y == y){
                 console.log('There is a ship at these coordinates', {x, y})
-            }
-        }
-
-        // Searching all my ships in the fleet
-        for (let boat of this.fleet){
-            // console.log(boat.coords)
-        // Searching all of the coordinates on all of my ships
-        // Then attacking the ship that matches the searched up coordinates
-            for (let info of boat.coords){
-                // console.log(info)
-                if (info.x == x && info.y == y){
-                    console.log(`${boat.name} has been hit`)
-                    boat.hit()
-                }
+                console.log(`${space.theShip.name} has been hit`)
+                space.theShip.hit()
             }
         }
     }
